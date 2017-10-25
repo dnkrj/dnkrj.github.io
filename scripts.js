f216c4b737c4fb772bae2b4c08f91a8a35d3d798
Daniel Karaj <hey@krj.io>
2017-10-25 16:59:22 +0200

    Change from hidden attribute to class

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', () => {
-	document.querySelector('.horizontal').hidden = document.body.scrollTop > document.body.scrollLeft;
-	document.querySelector('.vertical').hidden = document.body.scrollLeft > document.body.scrollTop;
+	document.querySelector('.horizontal').classList.toggle('isHidden', document.body.scrollTop > document.body.scrollLeft);
+	document.querySelector('.vertical').classList.toggle('isHidden', document.body.scrollLeft > document.body.scrollTop);
 });
