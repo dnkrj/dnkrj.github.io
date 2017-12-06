2dd991787943bd1af162f4d5e5747e54c7b705a1
Daniel Karaj <hey@krj.io>
2017-12-06 10:16:50 +0100

    Accessible colors and photo background

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 window.addEventListener('scroll', () => {
 	document.querySelector('.horizontal').classList.toggle('isHidden', document.body.scrollTop > document.body.scrollLeft);
 	document.querySelector('.vertical').classList.toggle('isHidden', document.body.scrollLeft > document.body.scrollTop);
+	document.querySelector('.background').style.opacity = Math.max(document.body.scrollLeft / 5 - 100, 0) / 100;
 });
