1c87f9c14b021dc4ca68512035adea7fc30b6c98
Daniel Karaj <hey@krj.io>
2018-06-12 22:42:06 +0200

    New styling

--- scripts.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', () => {
-	document.querySelector('.horizontal').classList.toggle('isHidden', window.scrollY > window.scrollX);
-	document.querySelector('.vertical').classList.toggle('isHidden', window.scrollX > window.scrollY);
-	document.querySelector('.background').style.opacity = Math.max(window.scrollX / 5 - 100, 0) / 100;
-});
