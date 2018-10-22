e857f974b215cbcb26abbc8cf139686af186a705
Daniel Karaj <dnkrj@dknrj-macbookair.roam.corp.google.com>
2018-10-22 11:41:53 +0200

    Progress

--- semiautocorrect/scripts.js
+++ semiautocorrect/scripts.js
@@ -1,4 +1,7 @@
+const BREAK_CHARACTERS = new Set([' ', '.', ',', '!', '?']);
+
 const input = document.querySelector('input');
+
 const messages = [
   [..."Type messages in the input below."],
   [..."Use ⏎ to send the message."],
@@ -6,12 +9,11 @@ const messages = [
   [..."Try correcting this mistke."],
 ];
 
-const BREAK_CHARACTERS = new Set([' ', '.', ',', '!', '?']);
-
 const corrections = [[], [], [], []];
 corrections[2].push({start: 38, end: 50});
 
-const mergeIntervals = correction => {
+const mergeCorrections = (correction) => {
+  correction.sort((a, b) => a.start - b.start || a.end - b.end);
   const result = [];
   let last;
   correction.forEach(c => {
@@ -43,18 +45,21 @@ const search = (message, correction) => {
 
     let a = 0;
     for (a; a < correction.length; a++) {
-      if (BREAK_CHARACTERS.has(message[start + correction.length + a]) || start + correction.length + a > message.length) {
+      if (BREAK_CHARACTERS.has(message[start + correction.length + a])
+          || start + correction.length + a > message.length) {
         break;
       }
-      if (BREAK_CHARACTERS.has(message[start + correction.length - a]) || start + correction.length - a > 0) {
+      if (BREAK_CHARACTERS.has(message[start + correction.length - a])
+          || start + correction.length - a < 0) {
         a = -a;
         break;
       }
       score++;
     }
 
-    let end = start + correction.length + Math.abs(a);
-    if (start + correction.length + a > message.length && BREAK_CHARACTERS.has(message[message.length - 1])) {
+    let end = start + correction.length + a;
+    if (start + correction.length + a > message.length
+        && BREAK_CHARACTERS.has(message[message.length - 1])) {
       end = message.length - 1;
     }
     if (score > result.score) {
@@ -63,6 +68,27 @@ const search = (message, correction) => {
     result = {start, end, score};
   }
   return result;
+}
+
+const send = (message) => {
+  const element = document.createElement('p');
+  element.classList.add('message');
+  element.textContent = message.join('');
+  document.querySelector('.messages').insertBefore(element, input);
+  messages.push(message);
+  corrections.push([]);
+}
+
+const findCorrection = (correction) => {
+  const searches = messages.map(m => search(m, correction));
+  const scores = searches.map((result) => result.score);
+  const lowestScore = Math.min(...scores);
+  const index = scores.lastIndexOf(lowestScore);
+  return {...searches[index], index};
+}
+
+const updateElementCorrections = (element) => {
+  
 }
 
 const submit = (event) => {
@@ -78,39 +104,35 @@ const submit = (event) => {
   input.value = '';
 
   if (message[0] != '*') {
-    const element = document.createElement('p');
-    element.classList.add('message');
-    element.textContent = message.join('');
-    document.querySelector('.messages').insertBefore(element, input);
-    messages.push(message);
-    corrections.push([]);
+    send(message);
     return;
   }
 
   const correction = message.slice(1);
-  const elements = [...document.querySelectorAll('.message')];
-  const searches = messages.map(m => search(m, correction));
-  const scores = searches.map((result) => result.score);
-  const lowestScore = Math.min(...scores);
-  const index = scores.lastIndexOf(lowestScore);
-  const {start, end} = searches[index];
-  messages[index] = messages[index].slice(0, start).concat(correction, messages[index].slice(end));
-  const element = elements[index];
+  const {start, end, index} = findCorrection(correction);
+
+  messages[index] =
+      messages[index]
+          .slice(0, start).concat(correction, messages[index].slice(end));
+
+  corrections[index].push({start, end: (start + correction.length)});
+  corrections[index] = mergeCorrections(corrections[index]);
+
+  const element = [...document.querySelectorAll('.message')][index];
   element.innerHTML = '';
-  const c = corrections[index];
-  c.push({start, end});
-  c.sort((a, b) => a.start - b.start || a.end - b.end);
-  corrections[index] = mergeIntervals(c);
+
   let last = 0;
   corrections[index].forEach(c => {
-    const textNode = document.createTextNode(messages[index].slice(last, c.start).join(''));
+    const textNode =
+        document.createTextNode(messages[index].slice(last, c.start).join(''));
     element.appendChild(textNode);
     const correctionNode = document.createElement('em');
     correctionNode.textContent = messages[index].slice(c.start, c.end).join('');
     element.appendChild(correctionNode);
     last = c.end;
   });
-  const textNode = document.createTextNode(messages[index].slice(last).join(''));
+  const textNode =
+    document.createTextNode(messages[index].slice(last).join(''));
   element.appendChild(textNode);
 }
 
